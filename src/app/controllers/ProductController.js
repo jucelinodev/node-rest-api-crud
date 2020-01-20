9b0478e5753bee0a4aa55a8e42cbf53b756d94cd
Jucelino Novais <jucelinodev@gmail.com>
2020-01-20 17:33:16 -0300

    check if product already exists before registering

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -4,6 +4,12 @@ class ProductController {
   async store (req, res) {
     const { name, description, category, price } = req.body
 
+    const productExist = await Product.findOne({ where: { name } })
+
+    if (productExist) {
+      return res.status(404).json({ error: 'Product already registered' })
+    }
+
     const product = await Product.create({ name, description, category, price })
 
     return res.status(201).json(product)
